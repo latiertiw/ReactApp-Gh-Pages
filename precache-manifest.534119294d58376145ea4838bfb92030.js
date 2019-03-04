081b3c2d47e67d545d2336086654d6eae6e42445
latiertiw <latierti@yandex.ru>
2019-03-04 09:10:05 +0300

    Updates

---
+++ precache-manifest.534119294d58376145ea4838bfb92030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d60515bb58dfa57be83",
+    "url": "/ReactApp-Gh-Pages/static/js/runtime~main.6d60515b.js"
+  },
+  {
+    "revision": "55989c9f63064ee6beec",
+    "url": "/ReactApp-Gh-Pages/static/js/main.55989c9f.chunk.js"
+  },
+  {
+    "revision": "184579dc1d6f58eebe40",
+    "url": "/ReactApp-Gh-Pages/static/js/2.184579dc.chunk.js"
+  },
+  {
+    "revision": "55989c9f63064ee6beec",
+    "url": "/ReactApp-Gh-Pages/static/css/main.1a120af8.chunk.css"
+  },
+  {
+    "revision": "2bdaf7486ac9a139f18e3efdbe3f4567",
+    "url": "/ReactApp-Gh-Pages/index.html"
+  }
+];
